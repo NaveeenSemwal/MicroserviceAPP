ceb64cb9aced2481031d45717c964ab28a22d91e
Naveen Semwal <semwal.dotnet@gmail.com>
2020-11-19 21:04:16 +0530

    Added EventBusRabbitMQ class library

---
+++ EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,50 @@
+﻿using EventBusRabbitMQ.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        public void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+            using (var channel = _connection.CreateModel())
+            {
+                var queue = channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var message = JsonSerializer.Serialize(publishModel);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+
+                channel.ConfirmSelect();
+                channel.BasicPublish(exchange: "", routingKey: queueName, mandatory: true, basicProperties: properties, body: body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += Channel_BasicAcks;
+
+                channel.ConfirmSelect();
+            }
+        }
+
+        private void Channel_BasicAcks(object sender, RabbitMQ.Client.Events.BasicAckEventArgs e)
+        {
+            Console.WriteLine("Sent RabbitMQ");
+            //implement ack handle
+        }
+    }
+}
